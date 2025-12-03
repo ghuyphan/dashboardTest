911aafde16b38f82832d141bd9f734865bf3ef58
LA - PHAN GIA HUY <pgh001-pxl-la@fhmc.com>
2025-12-03 07:38:04 +0700

    feat: update login styling and improve theme transitions

--- src/app/features/auth/login/login.component.spec.ts
+++ src/app/features/auth/login/login.component.spec.ts
@@ -1,16 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../../core/services/auth.service';
+import { ToastService } from '../../../core/services/toast.service';
+import { ModalService } from '../../../core/services/modal.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  const authServiceMock = {
+    login: jasmine.createSpy('login').and.returnValue(of({}))
+  };
+
+  const toastServiceMock = {
+    showSuccess: jasmine.createSpy('showSuccess'),
+    showError: jasmine.createSpy('showError')
+  };
+
+  const modalServiceMock = {
+    open: jasmine.createSpy('open')
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [LoginComponent, RouterTestingModule],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: ToastService, useValue: toastServiceMock },
+        { provide: ModalService, useValue: modalServiceMock }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
